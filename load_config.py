ce158516d1d32d17b5f276097a2634bc78421cbe
Irvin <irvinhomem@gmail.com>
2020-07-21 13:15:21 +0200

    Testing

--- load_config.py
+++ load_config.py
@@ -1,1 +1,11 @@
-def read_config()
+import hydra
+from omegaconf import DictConfig
+
+@hydra.main(config_path = "config/config.yaml")
+def load_config(cfg: DictConfig) -> None:
+    print(cfg.pretty())
+    
+if __name__ == "__main__":
+    load_config()
+
+# def read_config()
