efd0ca1626b09f9933a3fb60c6e8b9fad0ba4724
Irvin <irvinhatwork@gmail.com>
2020-07-21 14:22:31 +0200

    Class creation and initialization of logging

---
+++ xfe.py
@@ -0,0 +1,13 @@
+import logging
+
+class Xfe(object):
+
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        self.xfe_config = 
+
+def main(parameter_list):
+        xfe_test = Xfe()
