9655e36736bf4f0beee144fb4be05a6ea13b4fce
Irvin <irvinhatwork@gmail.com>
2020-07-22 13:41:41 +0200

    Fixed config load

--- ConfigManager.py
+++ ConfigManager.py
@@ -1,5 +1,6 @@
-import hydra
-from omegaconf import DictConfig
+# import hydra
+# from omegaconf import DictConfig
+from omegaconf import OmegaConf
 import logging
 import hydra_get_config
 
@@ -10,19 +11,30 @@ class ConfigManager(object):
         logging.basicConfig(level=logging.INFO)
         # logging.basicConfig(level=logging.WARNING)
         self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.INFO)
-        # self.logger.setLevel(logging.DEBUG)
+        # self.logger.setLevel(logging.INFO)
+        self.logger.setLevel(logging.DEBUG)
         # self.logger.setLevel(logging.WARNING)
 
         self.logger.info('Initializing Config Manager.')
-        self.local_config = self.load_config()
-        # self.api_user = self.local_config.get_full_key('api_key')
-        # self.api_pass =
+        # self.local_config = self.load_config()
+        self.config = OmegaConf.load("config/config.yaml")
+        self.logger.debug('self.local_config - TYPE: %s', type(self.config))
+
+        self.api_key = self.config['xfe']['api_key']
+        self.logger.debug('API key: %s', self.api_key)
+        self.api_pass = self.config['xfe']['api_pass']
+        self.logger.debug('API pass: %s', self.api_pass)
 
     def load_config(self):
         my_config = hydra_get_config.hydra_get_config()
         self.logger.info('TYPE: %s', type(my_config))
         return my_config
+
+    def get_api_key(self):
+        return self.api_key
+
+    def get_api_pass(self):
+        return self.api_pass
 
     # @hydra.main(config_path="config/config.yaml")
     # def load_config_hydra(self, cfg: DictConfig) -> None:
--- xfe.py
+++ xfe.py
@@ -7,12 +7,14 @@ class Xfe(object):
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
-        # self.logger.setLevel(logging.DEBUG)
-        self.logger.setLevel(logging.INFO)
+        self.logger.setLevel(logging.DEBUG)
+        # self.logger.setLevel(logging.INFO)
 
         self.logger.info('Initializing XFE')
         self.xfe_config = ConfigManager.ConfigManager()
-        self.logger.info('API USER: %s', self.xfe_config.xfe.api_key)
+        self.logger.debug('API key: %s', self.xfe_config.get_api_key())
+        self.logger.debug('API pass: %s', self.xfe_config.get_api_pass())
+        #self.logger.info('API USER: %s', self.xfe_config.xfe.api_key)
 
 
 def main():
